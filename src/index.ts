acc4b750ac2cbe50a08c46e6712417e79d6a8203
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-10 20:59:33 -0300

    setting up the server to allow cookies

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,78 @@ import express from "express";
 import {createServer} from "http";
 import {SubscriptionServer} from "subscriptions-transport-ws";
 import {subscribe, execute} from "graphql";
-import {schemaIndex} from "./resolvers";
+import cors from "cors";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
+import {schemaIndex} from "./resolvers";
 import {connection} from "./connection";
 
 const main = async () => {
   await connection();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient({
+    host: "127.0.0.1",
+    port: 6379,
+  });
+
   const app = express();
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: [
+        "https://studio.apollographql.com",
+        "http://localhost:4000/graphql",
+        "http://localhost:3000",
+      ],
+    })
+  );
+
+  app.set("trust proxy", true);
 
   const http = createServer(app);
 
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 24 * 60 * 60 * 60,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      },
+      saveUninitialized: false,
+      secret: "qiwroasdjlasddde",
+      resave: false,
+    })
+  );
+
+  redisClient.on("error", function (error) {
+    console.error(error);
+  });
+
   const schema = await schemaIndex;
 
-  const server = new ApolloServer({schema, introspection: true});
+  const server = new ApolloServer({
+    schema,
+    introspection: true,
+    context: ({req, res}) => ({
+      req,
+      res,
+      redis,
+    }),
+  });
 
   await server.start();
 
-  server.applyMiddleware({app, path: "/graphql"});
+  server.applyMiddleware({app, path: "/graphql", cors: false});
 
   SubscriptionServer.create(
     {schema, subscribe, execute},
