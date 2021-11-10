154ac372125cd4ea33c19cd173b091555557f427
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-10 20:58:26 -0300

    adding subscrioption options into assists mutation

--- src/resolvers/mutations/assists.mutation.ts
+++ src/resolvers/mutations/assists.mutation.ts
@@ -1,4 +1,4 @@
-import {Arg, Mutation, Resolver} from "type-graphql";
+import {Arg, Mutation, PubSub, PubSubEngine, Resolver} from "type-graphql";
 
 import {AssistsResponse} from "../../errors/assists.response";
 import {Assists} from "../../entity/assists.entity";
@@ -8,9 +8,11 @@ import {AssistsInput} from "../../inputs/assists.input";
 export class AssistsMutation {
   @Mutation(() => AssistsResponse, {nullable: true})
   async createInvitation(
-    @Arg("options", () => AssistsInput) options: AssistsInput
+    @Arg("options", () => AssistsInput) options: AssistsInput,
+    @PubSub() pubsub: PubSubEngine
   ): Promise<AssistsResponse> {
     const assists = await Assists.create({...options}).save();
+    pubsub.publish("NEW_INVITATION", assists);
 
     return {assists};
   }
