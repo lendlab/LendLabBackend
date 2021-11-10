759ae19fbd2ced0a18b84d9f691cbf36531f6d51
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-10 20:57:36 -0300

    adding login, me query and logout options to user

--- src/resolvers/mutations/user.mutation.ts
+++ src/resolvers/mutations/user.mutation.ts
@@ -1,16 +1,78 @@
-import {Arg, Mutation, Resolver} from "type-graphql";
+import {Arg, Ctx, Mutation, Resolver} from "type-graphql";
 
 import {User} from "../../entity/user.entity";
-import {UserInput} from "../../inputs/user.input";
+import {CiPasswordInput, UserInput} from "../../inputs/user.input";
 import {UserResponse} from "../../errors/user.response";
+import {MyContext} from "../../types/MyContext";
+import {getRepository} from "typeorm";
+import argon2 from "argon2";
 
 @Resolver()
 export class UserMutations {
   @Mutation(() => UserResponse)
+  async login(
+    @Arg("options", () => CiPasswordInput) options: CiPasswordInput,
+    @Ctx() {req}: MyContext
+  ): Promise<UserResponse> {
+    const user = await getRepository(User)
+      .createQueryBuilder("user")
+      .where(`user.ci = ${options.ci}`)
+      .getOne();
+
+    if (!user) {
+      return {
+        errors: [
+          {path: "ci", message: "Esta cedula no existe, intentalo de nuevo."},
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            path: "password",
+            message: "Contrasñea incorrecta, intentalo de nuevo.",
+          },
+        ],
+      };
+    }
+
+    console.log(user);
+
+    req.session.ci = user.ci;
+    return {user};
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() {req, res}: MyContext): Promise<unknown> {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie("qid");
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      })
+    );
+  }
+
+  @Mutation(() => UserResponse, {nullable: true})
   async createUser(
     @Arg("options", () => UserInput) options: UserInput
   ): Promise<UserResponse> {
-    const user = await User.create({...options}).save();
+    const hashed_pass = await argon2.hash(options.password);
+
+    const user = await User.create({
+      ci: options.ci,
+      email: options.email,
+      name: options.name,
+      password: hashed_pass,
+    }).save();
 
     return {user};
   }
--- src/resolvers/querys/user.query.ts
+++ src/resolvers/querys/user.query.ts
@@ -1,4 +1,7 @@
-import {Query, Resolver} from "type-graphql";
+import {MyContext} from "../../types/MyContext";
+import {Ctx, Query, Resolver} from "type-graphql";
+import {getRepository} from "typeorm";
+
 import {User} from "../../entity/user.entity";
 
 @Resolver()
@@ -6,5 +9,21 @@ export class UserQuery {
   @Query(() => [User])
   async getUsers() {
     return User.find();
+  }
+
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() {req}: MyContext) {
+    if (!req.session.ci) {
+      return null;
+    }
+
+    const ci = req.session.ci;
+
+    const user = getRepository(User)
+      .createQueryBuilder("user")
+      .where(`user.ci = ${ci}`)
+      .getOne();
+
+    return user;
   }
 }
