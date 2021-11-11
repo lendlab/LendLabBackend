8554d0daad1379aca7b854b3a79678126017022e
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-11 14:55:09 -0300

    new query

--- src/resolvers/querys/assissts.query.ts
+++ src/resolvers/querys/assissts.query.ts
@@ -1,6 +1,8 @@
-import {Query, Resolver} from "type-graphql";
+import {Ctx, Query, Resolver} from "type-graphql";
 import {getRepository} from "typeorm";
+
 import {Assists} from "../../entity/assists.entity";
+import {MyContext} from "../../types/MyContext";
 
 @Resolver()
 export class AssistsQuery {
@@ -11,6 +13,46 @@ export class AssistsQuery {
       .innerJoinAndSelect("assists.user", "user")
       .innerJoinAndSelect("assists.meeting", "meeting")
       .getMany();
+
+    return invitations;
+  }
+
+  @Query(() => [Assists], {nullable: true})
+  async getAceptedInvitations() {
+    const invitations = getRepository(Assists)
+      .createQueryBuilder("assists")
+      .innerJoinAndSelect("assists.user", "user")
+      .innerJoinAndSelect("assists.meeting", "meeting")
+      .where("assists.accepted = true")
+      .getMany();
+
+    return invitations;
+  }
+
+  @Query(() => [Assists], {nullable: true})
+  async getPendingInvitations() {
+    const invitations = getRepository(Assists)
+      .createQueryBuilder("assists")
+      .innerJoinAndSelect("assists.user", "user")
+      .innerJoinAndSelect("assists.meeting", "meeting")
+      .where("assists.accepted = false")
+      .getMany();
+
+    return invitations;
+  }
+
+  @Query(() => [Assists], {nullable: true})
+  async getMyInvitations(@Ctx() {req}: MyContext) {
+    const invitations = getRepository(Assists)
+      .createQueryBuilder("assists")
+      .innerJoinAndSelect("assists.user", "user")
+      .innerJoinAndSelect("assists.meeting", "meeting")
+      .where(`user.ci = ${req.session.ci}`)
+      .getMany();
+
+    if (!invitations) {
+      return null;
+    }
 
     return invitations;
   }
