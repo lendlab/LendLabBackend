7753cc8b78d3b0f2c3e1046feb76baea6b26734d
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-11 08:28:21 -0300

    adding acceptInvitation mutation

--- src/resolvers/mutations/assists.mutation.ts
+++ src/resolvers/mutations/assists.mutation.ts
@@ -1,8 +1,9 @@
-import {Arg, Mutation, PubSub, PubSubEngine, Resolver} from "type-graphql";
+import {Arg, Int, Mutation, PubSub, PubSubEngine, Resolver} from "type-graphql";
 
 import {AssistsResponse} from "../../errors/assists.response";
 import {Assists} from "../../entity/assists.entity";
-import {AssistsInput} from "../../inputs/assists.input";
+import {Accept, AssistsInput} from "../../inputs/assists.input";
+import { getRepository } from "typeorm";
 
 @Resolver()
 export class AssistsMutation {
@@ -15,5 +16,31 @@ export class AssistsMutation {
     pubsub.publish("NEW_INVITATION", assists);
 
     return {assists};
+  }
+
+  @Mutation(() => Boolean,{nullable: true})
+  async acceptInvitation(
+    @Arg("assits_id", () => Int)assits_id: number,
+    @Arg("options", () => Accept)options:Accept): Promise<Boolean>{
+    await getRepository(Assists)
+    .createQueryBuilder("assists")
+    .innerJoinAndSelect("assists.meeting", "meeting")
+    .innerJoinAndSelect("assists.user", "user")
+    .where("user.ci = 123")
+    .getOne()
+
+    const accept_invitation = await Assists.update({assits_id}, options)
+
+    if(options.accepted === false){
+      await Assists.delete({assits_id}                                                                                                     )
+
+      console.log("borrada")
+    }
+
+    if(!accept_invitation){
+      console.log("nan")
+    }
+
+    return true
   }
 }
