399deea015a3a2fe1f488cde4cb8b8d21b3a7720
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-11 08:25:51 -0300

    adding accepted column

--- src/entity/assists.entity.ts
+++ src/entity/assists.entity.ts
@@ -1,11 +1,14 @@
 import {Field, ObjectType} from "type-graphql";
-import {BaseEntity, Entity, ManyToOne} from "typeorm";
+import {BaseEntity, Column, Entity, ManyToOne} from "typeorm";
 import {User} from "./user.entity";
 import {Meeting} from "./meeting.entity";
 
 @ObjectType()
 @Entity()
 export class Assists extends BaseEntity {
+  @Column({default:1})
+  assits_id: number
+
   @Field(() => Meeting, {nullable: true})
   @ManyToOne(() => Meeting, (metting) => metting.assists, {
     primary: true,
@@ -19,4 +22,8 @@ export class Assists extends BaseEntity {
     onDelete: "CASCADE",
   })
   user: User;
+
+  @Field(() => Boolean)
+  @Column({default: 0})
+  accepted?: boolean
 }
