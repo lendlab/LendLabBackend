fd1818c2e5d368e65978f488ea0622e14b25ab26
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-11 14:56:02 -0300

    updating acceptInvitation mutation

--- src/resolvers/mutations/assists.mutation.ts
+++ src/resolvers/mutations/assists.mutation.ts
@@ -1,9 +1,18 @@
-import {Arg, Int, Mutation, PubSub, PubSubEngine, Resolver} from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Int,
+  Mutation,
+  PubSub,
+  PubSubEngine,
+  Resolver,
+} from "type-graphql";
 
 import {AssistsResponse} from "../../errors/assists.response";
 import {Assists} from "../../entity/assists.entity";
 import {Accept, AssistsInput} from "../../inputs/assists.input";
-import { getRepository } from "typeorm";
+import {getRepository} from "typeorm";
+import {MyContext} from "../../types/MyContext";
 
 @Resolver()
 export class AssistsMutation {
@@ -18,29 +27,33 @@ export class AssistsMutation {
     return {assists};
   }
 
-  @Mutation(() => Boolean,{nullable: true})
+  @Mutation(() => Boolean, {nullable: true})
   async acceptInvitation(
-    @Arg("assits_id", () => Int)assits_id: number,
-    @Arg("options", () => Accept)options:Accept): Promise<Boolean>{
-    await getRepository(Assists)
-    .createQueryBuilder("assists")
-    .innerJoinAndSelect("assists.meeting", "meeting")
-    .innerJoinAndSelect("assists.user", "user")
-    .where("user.ci = 123")
-    .getOne()
+    @Arg("assits_id", () => Int) assits_id: number,
+    @Arg("options", () => Accept) options: Accept,
+    @Ctx() {req}: MyContext
+  ): Promise<Boolean> {
+    const user = await getRepository(Assists)
+      .createQueryBuilder("assists")
+      .innerJoinAndSelect("assists.meeting", "meeting")
+      .innerJoinAndSelect("assists.user", "user")
+      .where(`user.ci = ${req.session.ci}`)
+      .getOne();
 
-    const accept_invitation = await Assists.update({assits_id}, options)
+    console.log(user);
 
-    if(options.accepted === false){
-      await Assists.delete({assits_id}                                                                                                     )
+    const accept_invitation = await Assists.update({assits_id}, options);
 
-      console.log("borrada")
+    if (options.accepted === false) {
+      await Assists.delete({assits_id});
+
+      console.log("borrada");
     }
 
-    if(!accept_invitation){
-      console.log("nan")
+    if (!accept_invitation) {
+      console.log("nan");
     }
 
-    return true
+    return true;
   }
 }
