9c7be32c7610129dd18b9d6f32b9b6f33cc747c8
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-11 14:54:27 -0300

    adding new querys

--- src/resolvers/mutations/meeting.mutation.ts
+++ src/resolvers/mutations/meeting.mutation.ts
@@ -1,16 +1,28 @@
-import {Arg, Int, Mutation, Resolver} from "type-graphql";
+import {Arg, Ctx, Int, Mutation, Resolver} from "type-graphql";
 
 import {Meeting} from "../../entity/meeting.entity";
 import {MeetingResponse} from "../../errors/meeting.response";
 import {MeetingInput} from "../../inputs/meeting.input";
+import {MyContext} from "../../types/MyContext";
 
 @Resolver()
 export class MeetingMutation {
   @Mutation(() => MeetingResponse, {nullable: true})
   async createMeeting(
-    @Arg("options", () => MeetingInput) options: MeetingInput
+    @Arg("options", () => MeetingInput) options: MeetingInput,
+    @Ctx() {req}: MyContext
   ): Promise<MeetingResponse> {
-    const meeting = await Meeting.create({...options}).save();
+    const user_ci = req.session.ci;
+
+    console.log(user_ci);
+
+    const meeting = await Meeting.create({
+      meeting_title: options.meeting_title,
+      meeting_description: options.meeting_description,
+      meeting_duration: options.meeting_duration,
+      create_at: options.create_at,
+      user: options.user,
+    }).save();
 
     return {meeting};
   }
--- src/resolvers/querys/meeting.query.ts
+++ src/resolvers/querys/meeting.query.ts
@@ -1,10 +1,16 @@
 import {Query, Resolver} from "type-graphql";
+import {getRepository} from "typeorm";
 import {Meeting} from "../../entity/meeting.entity";
 
 @Resolver()
 export class MeetingQuery {
   @Query(() => [Meeting], {nullable: true})
   async getMeetings() {
-    return Meeting.find();
+    const meetings = await getRepository(Meeting)
+      .createQueryBuilder("meeting")
+      .innerJoinAndSelect("meeting.user", "user")
+      .getMany();
+
+    return meetings;
   }
 }
